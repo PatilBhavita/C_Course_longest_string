508dee36fb318bc14f39d5fe1717308bf55b5aff
bhavitapatil <bhavitapatil12@gmail.com>
2022-05-11 20:44:21 +0530

    changes in programm

--- longest_string.c
+++ longest_string.c
@@ -3,18 +3,20 @@
 #include <string.h>
 
 int main(int argc, char* argv[]) {
+    char *longestString= "";
     int index;
-    if (argc <= 1) {
-        puts("empty string");
+    if (argc > 1)
+    {
+       index = 1;
+       for (int i = 2; i < argc; i++)
+       {
+         if (strlen(argv[index]) < strlen(argv[i]))
+         {
+             index = i;
+         }
+       }
+       longestString = argv[index];
     }
-    else {
-        index = 1;
-        for (int i = 2; i < argc; i++) {
-            if (strlen(argv[index]) < strlen(argv[i])) {
-                index = i;
-            }
-        }
-        puts(argv[index]);
-    }
+    puts(longestString);
     return EXIT_SUCCESS;
 }
